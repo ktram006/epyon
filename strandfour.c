8af13be6bd8d1d8acccd748213b57b49a81b308f
Kellen Burke Richardson <108907118+ktram006@users.noreply.github.com>
2022-07-15 13:22:48 -0400

    Add files via upload

---
+++ strandfour.c
@@ -0,0 +1,22 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (void) 
+{
+	srand((unsigned int)(time(NULL)));
+	int index = 0;
+
+	char char1[] =
+     "MN!@#BVCXZ098?'[]|qazws^xedc"
+	 "$%765A^*&SDFG4HJKLvfrtg"
+     "bnhy*-=_+ujm3lkpo.,:;><`~i21";
+
+	for(index = 0; index < 2048; index++)
+	{
+		printf("%c", char1[rand() % (sizeof char1 - 1)]);
+	}
+	puts("");
+	return 0;
+}
