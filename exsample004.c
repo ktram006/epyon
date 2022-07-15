2e89c02a4fbdc9f39e8a8112f568c5f61f7fee71
Kellen Burke Richardson <108907118+ktram006@users.noreply.github.com>
2022-07-15 13:21:02 -0400

    Add files via upload

---
+++ exsample004.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t now;
+    struct tm *today;
+    
+    time(&now);
+    today = localtime(&now);
+  
+    int currentday = today->tm_wday;
+    
+    printf("tm_wday=%d",
+            currentday  );
+}
---
+++ randstring.c
@@ -0,0 +1,32 @@
+﻿ //  THANKS FOR THE HELP EssGeEich!  //
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LEN 32;
+
+static const char alphanum[] =
+  "0123456789"
+  "!@#$%^&*-=?:;.,[]~|()_+'/{}`<>"
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "abcdefghijklmnopqrstuvwxyz";
+
+int stringLength = sizeof(alphanum) - 1;
+
+char genRandom(void)
+{
+    return alphanum[rand() % stringLength];
+}
+
+int main()
+{
+	void *memchr(const void *str, int c, size_t);
+    srand(time(0));
+    char Str[LEN];
+for(int i = 0; i <LEN; i++){Str[i]=genRandom()};
+  
+    printf("%s", Str);
+    
+}
