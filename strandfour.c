cc40a1441cabd0a440f1b90e1966e4378cdde213
Kellen Burke Richardson <108907118+ktram006@users.noreply.github.com>
2022-07-16 03:52:40 -0400

    Update strandfour.c

--- strandfour.c
+++ strandfour.c
@@ -1,22 +1,31 @@
-﻿
+﻿//  strand04.c                  //  KBR.2022
+//  pseudo random string generator
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main (void) 
-{
-	srand((unsigned int)(time(NULL)));
-	int index = 0;
+{   
+    //  seed Pseudo-Random engine w/ current time.
+    srand((unsigned int)(time(NULL)));
 
-	char char1[] =
-     "MN!@#BVCXZ098?'[]|qazws^xedc"
-	 "$%765A^*&SDFG4HJKLvfrtg"
-     "bnhy*-=_+ujm3lkpo.,:;><`~i21";
+    //  set integer index to 0
+    int index = 0;
 
+    char char1[] =               //  init char array:
+    "MN!@#BVCXZ098?'[]|ws^xedc5"    //  UPPER/lower
+    "$%76A^*&SDFqazG4HpoKLvfrtg"    //  alphanumeric
+    "bnhy*-=_+ujm3lk.,:;><`~i21";   //  +punctuation 
+
+        //  generates string of 2048 bytes from char array
 	for(index = 0; index < 2048; index++)
 	{
-		printf("%c", char1[rand() % (sizeof char1 - 1)]);
+          printf("%c", char1[rand() % (sizeof char1 - 1)]);
 	}
+
+        //  print result on stdout <1>
 	puts("");
-	return 0;
+
+	return 0;    //  exit successfully.
 }
